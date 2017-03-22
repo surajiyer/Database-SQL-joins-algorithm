159104b409f2de660a9987ce534323abac0d1138
steef2404 <steef2404@hotmail.com>
2017-03-22 16:48:03 +0100

    Algorithm1 updated because offset was not working correct and made lengths random for now so it is a bit more interesting

---
+++ QueryGraph.py
@@ -0,0 +1,10 @@
+class queryTree:
+    def __init__(self, renames, joins):
+        print('\n')
+        for statement in joins:
+            stat = str(statement)
+            temp = [x.strip() for x in stat.split("=")]
+            table_name =
+            print("stat", splits[0])
+
+        #self.relations = []
