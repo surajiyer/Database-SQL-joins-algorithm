0b9e2036fd49b8ac117d2dd7281130e7731f9eab
suraj.iyer96@outlook.com <eurgh5616+>
2017-03-24 13:08:30 +0100

    Algorithm2

--- Algorithm2.py
+++ Algorithm2.py
@@ -6,24 +6,25 @@ import Algorithm1
 def estimate_query(G, b, n):
     samples = {}
     for R in G.getRelations():
-        samples[R] = sampleRelation(R, n)
-        print(samples[R])
+        R_set = frozenset({R})
+        samples[R_set] = sample_relation(R, n)
+        print(samples[R_set])
     budget = b
     for size in range(1, G.getRelations().size() - 1):
-        for (exp_in, Sin) in samples.getEntriesOfSize(size)
+        for (exp_in, Sin) in samples.get_entries_of_size(size)
             for R in G.getNeighbours(exp_in)
                 exp_out = exp_in + R
                 if (samples[exp_out].size() < n / 10) and (R.hasIndex(exp_in) or len(R) <= n)):
                     S_out = Algorithm1.sampleIndex(S_in, R.getIndex(exp_in), n)
                 samples[exp_out] = S_out
-                budget -= sampleCost(S_in, S_out, R)
+                budget -= sample_cost(S_in, S_out, R)
                 if budget < 0:
                     return samples
     return samples
 
 
-def sampleRelation(R, n):
-    return data.sampleTable(R, n)
+def sample_relation(R, n):
+    return data.sample_table(R, n)
 
 
 queries = tuple(open('join-order-benchmark/allqueries.sql', 'r'))
--- QueryGraph.py
+++ QueryGraph.py
@@ -1,10 +1,45 @@
-class queryTree:
-    def __init__(self, renames, joins):
-        print('\n')
-        for statement in joins:
-            stat = str(statement)
-            temp = [x.strip() for x in stat.split("=")]
-            table_name =
-            print("stat", splits[0])
+import pandas as pd
 
-        #self.relations = []
+
+class QueryGraph:
+    def __init__(self, tables, selects, joins):
+        self.V = []
+        self.E = dict()
+
+        for op in selects:
+            # TODO: call function here to calculate pandas dfs for selects
+            self.V.append(op)
+
+
+
+    def get_relations(self):
+        return self.V
+
+    def get_neighbors(self, R):
+        assert isinstance(R, Relation), 'R must be a relation expression'
+
+
+class Relation:
+    def __init__(self, df):
+        assert isinstance(df, pd.DataFrame)
+        self.df = df
+
+    def get_index(self):
+        pass
+
+    def __setattr__(self, key, value):
+        if key in self.__dict__:
+            raise AttributeError('Cannot change constant attribute')
+        self.__dict__[key] = value
+
+
+class Predicate:
+    def __init__(self, R1, R2):
+        assert isinstance(R1, Relation) and isinstance(R2, Relation)
+        self.u = R1
+        self.v = R2
+
+    def __setattr__(self, key, value):
+        if key in self.__dict__:
+            raise AttributeError('Cannot change constant attribute')
+        self.__dict__[key] = value
