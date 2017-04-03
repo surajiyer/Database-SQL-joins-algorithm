3db0e04580a6299b1bb7c4c10de1ea0e8b9c6643
suraj.iyer96@outlook.com <eurgh5616+>
2017-04-03 17:22:42 +0200

    Small bug fix for algorithm 2

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from Algorithms import *
+from SelectParser import *
 
 # column_names = ['id', 'name', 'imdb_index', 'imdb_id', 'name_pcode_nf', 'surname_pcode', 'md5sum']
 # dates = pd.date_range('20130101', periods=6)
@@ -12,6 +13,5 @@ from Algorithms import *
 # print(result)
 
 if __name__ == "__main__":
-    from SelectParser import *
     qry = "SELECT MIN(mc.note) AS production_note, MIN(t.title) AS movie_title, MIN(t.production_year) AS movie_year FROM company_type AS ct, info_type AS it, movie_companies AS mc, movie_info_idx AS mi_idx, title AS t WHERE ct.kind = 'production companies' AND it.info = 'top 250 rank' AND mc.note  not like '%(as Metro-Goldwyn-Mayer Pictures)%' and (mc.note like '%(co-production)%' or mc.note like '%(presents)%') AND ct.id = mc.company_type_id AND t.id = mc.movie_id AND t.id = mi_idx.movie_id AND mc.movie_id = mi_idx.movie_id AND it.id = mi_idx.info_type_id;"
     G = QueryGraph(get_tables(qry), *get_where(qry))
